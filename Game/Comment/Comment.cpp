96234bceae60ea1fd00fcf8fcaf4cb4d1a80b446
yifne0 <yifne0@outlook.jp>
2016-05-08 15:07:50 +0900

    Revert "コメントシステム ver 0.3 "first commit""
    This reverts commit ab81a91c362e82100fd9ee8a610441771e96ac0b.

--- Game/Comment/Comment.cpp
+++ Game/Comment/Comment.cpp
@@ -1,22 +1,17 @@
 #include "Comment.h"
-#include "CommentTask/CommentTask.h"
 
 Comment::Comment()
 {
-    CommentTask::setup();
 }
 
 Comment::~Comment()
 {
-    CommentTask::clear();
 }
 
 void Comment::update()
 {
-    CommentTask::update();
 }
 
 void Comment::draw()
 {
-    CommentTask::draw();
 }
