d987839a4790a98672681f70c3b1c2c2bfa80829
Yudai Fukui <yu1229kotetu@gmail.com>
2016-05-24 12:01:09 +0900

    プレイヤーの歩きと死亡モーション追加 (#43)
    * アニメーション追加
    
    追加
    
    * CharaBaseバグ直し
    
    * プレイヤーの歩きと死亡モーション追加
    
    * アニメーション用のtxt追加

--- src/Game/Player/Player.h
+++ src/Game/Player/Player.h
@@ -11,15 +11,61 @@ public:
 	void update() override;
 
 	void setPos(Vec2f _pos);
-	void setSize(Vec2f _size);
 	Vec2f getPos();
+	void setSize(Vec2f _size);
 	Vec2f getSize();
+	WritableP<bool> is_dead;
 
-private:
+	enum CharaState {
+		STOP,
+		WALK,
+		DEAD,
 
-	void resourceRoad();
+		BOMB,
+		BOW,
+	};
+
+private: // キャラの状態変更
+	CharaState charastate = STOP;
+	void charastateUpdate();
+
 	void move();
-	void animation();
+private: // 物を持っているかどうか
+	bool is_having = false;
+
+
+private: // 死亡
+	bool is_dead_ = false;
+	void dead();
+	std::set<int> deadsize_change_timing;
+	int default_size = size.x();
+	void sizeReset();
+	void deadMotion();
+	int dead_count;
+	void deadSizeChange();
+
+private: // 画像読み込み
+	void resourceRoad();
+
+private: // アニメーション
+
+	std::unordered_map<std::string, int> animation_name;
+	int getAnimationTime(const std::string name_);
+
+	std::set<int> now_animation;
+	void animationChange();
+
+	void animationSetup();
+	void animationUpdate();
+	void animationDraw();
+
+private: // axis試し
+	float axis = 0;
+	float getAxis(const std::string& axis_);
+
+	std::unordered_map<std::string, int> axis_name;
+	void axisSetup();
+
 
 };
 
